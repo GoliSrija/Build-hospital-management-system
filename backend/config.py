66f1a3ff901e26fb2446c4f21099c980f6813917
GoliSrija <23251a1241@gmail.com>
2026-07-28 11:15:21 +0530

    Add backend/config.py

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+```python
+class Config:
+    SECRET_KEY = 'secret_key_here'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///hospital_management_system.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+```
